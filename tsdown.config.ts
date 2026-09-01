1c10121d03a1c1db276c08de6cce8c15fd57cdd8
Waren Gador <waren.gador15@gmail.com>
2026-09-02 02:33:47 +0800

    update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    entry: [
-       "src/index.ts"
-    ],
-    format: ["esm", "cjs"],
-    dts: true,
-    minify: true,
-    target: "esnext",
-    unbundle: true,
-    platform: "node",
+	entry: ["src/index.ts"],
+	outDir: "dist",
+	format: ["esm", "cjs"],
+	dts: true,
+	minify: true,
+	target: "esnext",
+	unbundle: true,
+	platform: "node",
 });
